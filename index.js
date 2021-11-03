4a4b8e137d04fb188f979ebb054fe76b0c204615
Przemysław Klejno <pemix09@onet.pl>
2021-11-03 23:55:05 +0100

    added working web scraping

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const port = 8000;
+const url = "https://www.theguardian.com/international";
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
+
+const app = express();
+
+axios(url).then(response =>{
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $('.fc-item__title',html).each(function(){
+        const title = $(this).text();
+        const ArticleUrl = $(this).find('a').attr('href');
+        articles.push({
+            title: title,
+            url: ArticleUrl
+        });
+    })
+}).catch(err => console.log(err));
+
+app.listen(port,() => console.log('server running on port: ' + port));
+
