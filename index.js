4884c1f66265d1ca915f672da96d5ed82422648e
Przemysław Klejno <pemix09@onet.pl>
2021-11-04 00:39:01 +0100

    The app forsk, fetches datat from backend and prints it to the console

--- index.js
+++ index.js
@@ -3,24 +3,43 @@ const url = "https://www.theguardian.com/international";
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
-axios(url).then(response =>{
 
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const articles = [];
+//http request methods
+//app.METHOD(PATH, handler)
 
-    $('.fc-item__title',html).each(function(){
-        const title = $(this).text();
-        const ArticleUrl = $(this).find('a').attr('href');
-        articles.push({
-            title: title,
-            url: ArticleUrl
-        });
-    })
-}).catch(err => console.log(err));
+app.get('/',function(req, res){
+    res.json('This is my awesome webScraper!');
+})
+
+app.get('/results',(req,res) =>{
+
+    axios(url).then(response =>{
+
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+    
+        $('.fc-item__title',html).each(function(){
+            const title = $(this).text();
+            const ArticleUrl = $(this).find('a').attr('href');
+            articles.push({
+                title: title,
+                url: ArticleUrl
+            });
+        })
+
+        res.json(articles);
+
+    }).catch(err => console.log(err));
+
+})
+
+
 
 app.listen(port,() => console.log('server running on port: ' + port));
 
---
+++ src/app.js
@@ -0,0 +1,1 @@
+fetch('http://localhost:8000/results').then(response => response.json()).then(data => console.log(data));
